0f84efef8196fcf95a3ab9c59551e460c0625917
Nikita <example@example.com>
2020-10-05 19:06:19 +0700

    dev app11

--- src/App.js
+++ src/App.js
@@ -7,17 +7,16 @@ export default class App extends React.Component{
 
   constructor(props) {
     super(props);
-    
-    const socket = io("https://chat2222.herokuapp.com/");
 
-    socket.on('message', function (data) {
+    this.socket = io("https://chat2222.herokuapp.com/");
+
+    this.socket.on('message', function (data) {
       const p = document.createElement('p');  
       p.textContent = data.msg;
       document.getElementById('chat').appendChild(p);
     });
   }
 
-  /*
   message() {
     const text = document.getElementById("text").value;
     this.socket.emit('message', {msg: text});
@@ -32,7 +31,7 @@ export default class App extends React.Component{
     } catch (error) {
 
     }
-  }*/
+  }
 
   render() {
     return (
@@ -40,7 +39,7 @@ export default class App extends React.Component{
         <div id="chat"></div>
         <div id="button">
           <input id="text" type="input" />
-          <button onClick="">Отправить</button>
+          <button onClick={this.message}>Отправить</button>
         </div>
       </div>
     );
